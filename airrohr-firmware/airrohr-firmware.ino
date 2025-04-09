4816f9bf705142eb83c5ad92acb7462e7bb924fe
kris <kris@git.local>
2025-04-09 20:10:52 +0300

    added a comment to trigger action

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -5339,6 +5339,7 @@ static void init_display()
 {
 	if (cfg::has_display)
 	{
+		//changed by me
 		oled_ssd1306 = new SSD1306(0x3c, 14, 12);
 		oled_ssd1306->init();
 		if (cfg::has_flipped_display)
